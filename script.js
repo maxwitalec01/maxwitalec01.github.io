037ab1a91f24c4e476a49725de27f25b9afc36a6
Max Witalec <mwitalec01@gmail.com>
2023-12-16 21:02:15 +0000

    working on education section

--- script.js
+++ script.js
@@ -108,3 +108,126 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var currentYear = 1; 
+  var maxYear = 4; 
+  var isAnimating = false;
+
+  var gradeData = {
+      1: [
+          { course: "Software Development 1", grade: "B+" },
+          { course: "Computer Architecture", grade: "B" },
+          { course: "Visual Design & User Exp", grade: "A" },
+          { course: "Business & Information Sys", grade: "B+" },
+          { course: "Discrete Mathematics 1", grade: "B" },
+          { course: "Critical Skills Development", grade: "B" },
+          { course: "Software Development 2", grade: "C+" },
+          { course: "Systems Analysis", grade: "C+" },
+          { course: "Social Media Communications", grade: "B+" },
+          { course: "Statistics", grade: "C+" },
+          { course: "Database Fundamentals", grade: "B+" },
+          { course: "Operating Syst Fundamentals", grade: "B" },
+          
+      ],
+      2: [
+          { course: "Software Development 3", grade: "B" },
+          { course: "Software Qual Ass & Testing", grade: "B" },
+          { course: "Network Fundementals", grade: "B+" },
+          { course: "Adv Database Technologies", grade: "B+" },
+          { course: "Discrete Mathematics 2", grade: "A" },
+          { course: "Client side Web Development", grade: "C+" },
+          { course: "Software Development 4", grade: "C+" },
+          { course: "Routing & Switching", grade: "B" },
+          { course: "Management Science", grade: "B+" },
+          { course: "Database Admin & Analysis", grade: "B" },
+          { course: "Information Security", grade: "B" },
+          { course: "Project", grade: "B" },
+          
+      ],
+      3:[
+        { course: "Server-side Web Development", grade: "B-" },
+        { course: "Operating Systems", grade: "B-" },
+        { course: "Cloud Service & Distrib Comp", grade: "C" },
+        { course: "Big Data Technologies", grade: "B-" },
+        { course: "Data Structures & Algorithms", grade: "B+" },
+        { course: "Data Analysis", grade: "C" },
+        { course: "EXPERIENTAL LEARNING", grade: "PASS" },
+
+      ]
+
+  };
+
+
+  var table = document.querySelector('.table');
+  var tableBody = table.querySelector('tbody');
+  var yearDisplay = document.getElementById('yearDisplay');
+
+  function updateTable(year, animate = false) {
+      if(isAnimating) return; // Prevent animation if one is already in progress
+      isAnimating = true;
+
+      // Update the year display
+      yearDisplay.innerText = `Year ${year}`;
+
+      function handleAnimationEnd() {
+          if (animate) {
+              // Clear existing table data after fade-out
+              tableBody.innerHTML = '';
+              table.classList.remove('fade-out');
+
+              // Populate table with new data
+              gradeData[year].forEach(function(item) {
+                  var row = `<tr><td>${item.course}</td><td>${item.grade}</td></tr>`;
+                  tableBody.innerHTML += row;
+              });
+
+              // Apply fade-in animation to the whole table
+              table.classList.add('fade-in');
+          } else {
+              // Immediate update without animation
+              tableBody.innerHTML = '';
+              gradeData[year].forEach(function(item) {
+                  var row = `<tr><td>${item.course}</td><td>${item.grade}</td></tr>`;
+                  tableBody.innerHTML += row;
+              });
+              isAnimating = false;
+          }
+      }
+
+      if (animate) {
+          // Apply fade-out animation to the whole table
+          table.classList.add('fade-out');
+          table.addEventListener('animationend', handleAnimationEnd, { once: true });
+      } else {
+          handleAnimationEnd();
+      }
+  }
+
+  // Reset the animation flag after fade-in
+  table.addEventListener('animationend', function() {
+      if (table.classList.contains('fade-in')) {
+          table.classList.remove('fade-in');
+          isAnimating = false;
+      }
+  });
+
+  // Event listeners for buttons
+  document.getElementById('prevYear').addEventListener('click', function() {
+      if (currentYear > 1 && !isAnimating) {
+          currentYear--;
+          updateTable(currentYear, true);
+      }
+  });
+
+  document.getElementById('nextYear').addEventListener('click', function() {
+      if (currentYear < maxYear && !isAnimating) {
+          currentYear++;
+          updateTable(currentYear, true);
+      }
+  });
+
+  // Initialize table with the first year without animation
+  updateTable(currentYear);
+});
